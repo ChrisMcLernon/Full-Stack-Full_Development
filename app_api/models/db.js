a066dd5d1f803be83414a522e421cd308564f2a9
Chris McLernon <93166075+Chris-McLernon@users.noreply.github.com>
2024-02-13 15:18:07 -0500

    On branch Module5  Your branch is up to date with 'origin/Module5'.
     Changes to be committed:
    	modified:   .seedgooserc.js
    	modified:   app.js
    	new file:   app_api/controllers/trips.js
    	modified:   app_api/models/db.js
    	new file:   app_api/routes/index.js
    	modified:   app_server/controllers/travel.js
    	modified:   app_server/routes/travel.js
    	modified:   app_server/views/travel.hbs
    	new file:   data/db/diagnostic.data/metrics.2024-02-13T14-41-27Z-00000
    	new file:   data/db/diagnostic.data/metrics.2024-02-13T19-38-41Z-00000
    	new file:   data/db/diagnostic.data/metrics.2024-02-13T19-49-46Z-00000
    	new file:   data/db/journal/WiredTigerLog.0000000004
    	modified:   package-lock.json
    	modified:   package.json

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,55 +1,62 @@
 const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = 'mongodb://${host}/travlr';
+const dbURL = `mongodb://${host}/travlr`;
 const readLine = require('readline');
 
-const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    } ), 1000);
+if (process.env.NODE_ENV === 'production') {
+  dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
 }
 
-// CONNECTION EVENTS
-mongoose.connection.on('connected', function() {
-    console.log('Mongoose connected to ' + dbURI);
-});
-mongoose.connection.on('error', function(err) {
-    console.log('Mongoose connection error: ' + err);
-});
-mongoose.connection.on('disconnected', function() {
-    console.log('Mongoose disconnected');
+const connect = () => {
+  setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }), 1000);
+}
+
+mongoose.connection.on('connected', () => {
+  console.log('connected');
 });
 
-// CAPTURE APP TERMINATION / RESTART EVENTS
-// To be called when process is restarted or terminated
-const gracefulShutdown = function(msg, callback) {
-    mongoose.connection.close(function() {
-        console.log('Mongoose disconnected through ' + msg);
-        callback();
-    });
+mongoose.connection.on('error', err => {
+  console.log('error: ' + err);
+  return connect();
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('disconnected');
+});
+
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit("SIGINT");
+  });
+}
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
 };
-// For nodemon restarts
-process.once('SIGUSR2', function() {
-    gracefulShutdown('nodemon restart', function() {
-        process.kill(process.pid, 'SIGUSR2');
-    });
+
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
 });
-// For app termination
-process.on('SIGINT', function() {
-    gracefulShutdown('app termination', function() {
-        process.exit(0);
-    });
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
 });
-// For Heroku app termination
-process.on('SIGTERM', function() {
-    gracefulShutdown('Heroku app termination', function() {
-        process.exit(0);
-    });
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
 });
 
 connect();
 
-// BRING IN YOUR SCHEMAS & MODELS
 require('./travlr');
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,13 +1,59 @@
-var fs = require('fs');
+const request = require('request');
+const apiOptions = {
+ server: 'http://localhost:3000'
+}
 
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+/* render travel list view */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
 
-/* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways', trips });
+    // if the result was not an array, make it an array and report error
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if (!responseBody.length) {
+            message = 'No trips exist in database';
+        }
+    }
+
+    // render the page
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
 };
-   
+
+
+/* GET travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    // construct the request
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    // send the request to the api and get a response. Use the response to render the travel page
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log('statusCode: ', res && res.statusCode)
+            renderTravelList(req, res, body);
+        }
+    )
+};
+
+
 module.exports = {
-    travel
-};
-   
+    travelList
+}
